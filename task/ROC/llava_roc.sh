cb8e7d65f327778f372c2ce7192fced51bfbb959
Mingrui Wu <59433903+mrwu-mac@users.noreply.github.com>
2025-01-08 10:53:44 +0800

    Update llava_roc.sh

--- task/ROC/llava_roc.sh
+++ task/ROC/llava_roc.sh
@@ -1,11 +1,11 @@
 # llava with box
-CUDA_VISIBLE_DEVICES=6,7 python task/ROC/llava_roc.py --visual_prompt='box' --answers_file='outputs/llava_7b_roc_box.json' --early_stop
+CUDA_VISIBLE_DEVICES=6,7 python task/ROC/llava_roc.py --visual_prompt='Box' --answers_file='outputs/llava_7b_roc_box.json' --early_stop
 
 # llava with mask
-# CUDA_VISIBLE_DEVICES=6,7 python task/ROC/llava_roc.py --visual_prompt='mask' --answers_file='outputs/llava_7b_roc_mask.json' --early_stop
+# CUDA_VISIBLE_DEVICES=6,7 python task/ROC/llava_roc.py --visual_prompt='Mask' --answers_file='outputs/llava_7b_roc_mask.json' --early_stop
 
 # llava with scribble
-# CUDA_VISIBLE_DEVICES=6,7 python task/ROC/llava_roc.py --visual_prompt='scribble' --answers_file='outputs/llava_7b_roc_scribble.json' --early_stop
+# CUDA_VISIBLE_DEVICES=6,7 python task/ROC/llava_roc.py --visual_prompt='Scribble' --answers_file='outputs/llava_7b_roc_scribble.json' --early_stop
 
 # llava with point
-# CUDA_VISIBLE_DEVICES=6,7 python task/ROC/llava_roc.py --visual_prompt='point' --answers_file='outputs/llava_7b_roc_point.json' --early_stop
+# CUDA_VISIBLE_DEVICES=6,7 python task/ROC/llava_roc.py --visual_prompt='Point' --answers_file='outputs/llava_7b_roc_point.json' --early_stop
